f28528fa6bccd380008f94c6752a1d4ccac0dc33
Christopher Wellons <wellons@nullprogram.com>
2014-11-26 15:36:51 -0500

    Add vga_putc().

--- vga.h
+++ vga.h
@@ -1,6 +1,20 @@
 #ifndef VGA_H
 #define VGA_H
 
+#define VGA_GWIDTH  320
+#define VGA_GHEIGHT 200
+#define VGA_TWIDTH  80
+#define VGA_THEIGHT 25
+
+static int vga_graphics()
+{
+#asm
+    mov   ah,#$00;
+    mov   al,#$13;
+    int   #$10
+#endasm
+}
+
 static void vga_set(int x, int y, int color)
 {
 #asm
@@ -20,21 +34,32 @@ static void vga_set(int x, int y, int color)
 #endasm
 }
 
-static int vga_enable()
-{
-#asm
-    mov   ah,#$00;
-    mov   al,#$13;
-    int   #$10
-#endasm
-}
-
-static int vga_disable()
+static int vga_text()
 {
 #asm
     mov   ah,#$00;
     mov   al,#$03;
     int   #$10
+#endasm
+}
+
+static void vga_putc(int x, int y, int c)
+{
+#asm
+  mov   bx,sp
+  mov   ax,[bx+4]  ! y
+  imul  ax,#80
+  mov   cx,[bx+2]  ! x
+  add   cx,ax      ! y*80+x
+  imul  cx,#2
+  mov   dx,[bx+6]  ! c
+
+  push  ds
+  mov   bx,#$B800  ! VGA text segment
+  mov   ds,bx
+  mov   bx,cx
+  mov   [bx],dx
+  pop   ds
 #endasm
 }
 
