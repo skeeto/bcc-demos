23e4056616c3da8366ae8d132cabc13bcef16787
Christopher Wellons <wellons@nullprogram.com>
2014-11-30 10:30:26 -0500

    Bring back old sleep.

--- gcc/time.h
+++ gcc/time.h
@@ -13,12 +13,26 @@ static uint32_t get_tick()
     return result;
 }
 
-static void msleep(uint32_t ms)
+/* Granularity of 976 usec and doesn't work in DOSBox. */
+static void usleep(uint32_t us)
 {
-    uint32_t us = ms * 1000;
     asm("mov   $0x86, %%ah\n"
         "int   $0x15\n"
         : /* no outputs */
         : "cx"(us >> 16), "dx"(us & 0x16)
         : "%ah", "%flags");
+}
+
+/* Granularity of 55 msec. */
+static void msleep(int ms)
+{
+    uint32_t count = ms / 54; // 18.2 Hz
+    uint32_t last = get_tick();
+    while (count) {
+        uint32_t now = get_tick();
+        if (now != last) {
+            last = now;
+            count--;
+        }
+    }
 }
