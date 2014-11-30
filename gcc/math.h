703d62e456c60c6e137907d1267330e3949c3a4b
Christopher Wellons <wellons@nullprogram.com>
2014-11-30 13:46:44 -0500

    Add min and max.

--- gcc/math.h
+++ gcc/math.h
@@ -6,4 +6,14 @@ static inline int abs(int x)
     return x < 0 ? -x : x;
 }
 
+static inline int max(int a, int b)
+{
+    return a < b ? b : a;
+}
+
+static inline int min(int a, int b)
+{
+    return b < a ? b : a;
+}
+
 #endif
