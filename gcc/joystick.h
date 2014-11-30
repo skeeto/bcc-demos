a4574700771078fe86b99ca43abe059bcfe8bced
Christopher Wellons <wellons@nullprogram.com>
2014-11-30 13:46:55 -0500

    Update joystick lib

--- gcc/joystick.h
+++ gcc/joystick.h
@@ -1,9 +1,16 @@
+#include "math.h"
+
 struct joystick {
-    int16_t x, y;
+    uint16_t x, y;
     bool a, b;
 };
 
-void joystick_read(struct joystick *a, struct joystick *b)
+static struct {
+    uint16_t min, max;
+} joystick_limits[2] = {{0xffff, 0x000}, {0xffff, 0x0000}};
+
+/* Can't be static for optimization reasons. */
+void joystick_read2(struct joystick *a, struct joystick *b)
 {
     asm("mov  $0x84, %%ah\n"
         "mov  $1, %%dx\n"
@@ -11,6 +18,14 @@ void joystick_read(struct joystick *a, struct joystick *b)
         : "=ax"(a->x), "=bx"(a->y), "=cx"(b->x), "=dx"(b->y)
         : /**/
         : /**/);
+    joystick_limits[0].min = min(joystick_limits[0].min, a->x);
+    joystick_limits[0].max = max(joystick_limits[0].max, a->x);
+    joystick_limits[0].min = min(joystick_limits[0].min, a->y);
+    joystick_limits[0].max = max(joystick_limits[0].max, a->y);
+    joystick_limits[1].min = min(joystick_limits[1].min, b->x);
+    joystick_limits[1].max = max(joystick_limits[1].max, b->x);
+    joystick_limits[1].min = min(joystick_limits[1].min, b->y);
+    joystick_limits[1].max = max(joystick_limits[1].max, b->y);
     uint16_t buttons = 0;
     asm("mov  $0x84, %%ah\n"
         "mov  $0, %%dx\n"
@@ -22,4 +37,10 @@ void joystick_read(struct joystick *a, struct joystick *b)
     a->b = !(buttons & (1 << 5));
     b->a = !(buttons & (1 << 6));
     b->b = !(buttons & (1 << 7));
+}
+
+void joystick_read(struct joystick *joystick)
+{
+    struct joystick dummy;
+    joystick_read2(joystick, &dummy);
 }
