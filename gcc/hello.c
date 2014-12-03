086ef5b30a863efd03059be7edbd42089a420321
Christopher Wellons <wellons@nullprogram.com>
2014-12-02 23:55:09 -0500

    Add joystick to demo.

--- gcc/hello.c
+++ gcc/hello.c
@@ -6,6 +6,7 @@ asm (".code16gcc\n"
 #include "int.h"
 #include "time.h"
 #include "vga.h"
+#include "joystick.h"
 
 int main(void)
 {
@@ -16,11 +17,23 @@ int main(void)
     vga_line((struct point){1, 198}, (struct point){318, 1}, 7);
     vga_line((struct point){317, 1}, (struct point){0, 198}, 8);
     vga_rect((struct rect){{120, 60}, {200, 140}}, 14);
-    msleep(1500);
-    for (int i = 0; i < 256; i++) {
-        vga_clear(i);
-        vga_vsync();
-        msleep(100);
+    msleep(1000);
+
+    struct joystick joy;
+    joystick_read(&joy);
+    if (joy.x != 0 || joy.y != 0) {
+        for (;;) {
+            joystick_read(&joy);
+            vga_vsync();
+            int min = joystick_limits[0].min, max = joystick_limits[0].max;
+            vga_clear(32 + ((joy.y - min) * 16) / max);
+        }
+    } else {
+        for (int i = 0; i < 256; i++) {
+            vga_clear(i);
+            vga_vsync();
+            msleep(100);
+        }
     }
     vga_off();
     return 1;
