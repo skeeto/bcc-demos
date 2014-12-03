c3d74861cab0dc3084aa5e727bfbfd16f2b917ae
Christopher Wellons <wellons@nullprogram.com>
2014-12-02 22:54:26 -0500

    Fix up joystick code.

--- gcc/joystick.h
+++ gcc/joystick.h
@@ -7,17 +7,16 @@ struct joystick {
 
 static struct {
     uint16_t min, max;
-} joystick_limits[2] = {{0xffff, 0x000}, {0xffff, 0x0000}};
+} joystick_limits[2] = {{0xffff, 0x0000}, {0xffff, 0x0000}};
 
-/* Can't be static for optimization reasons. */
-void joystick_read2(struct joystick *a, struct joystick *b)
+static void joystick_read2(struct joystick *a, struct joystick *b)
 {
-    asm("mov  $0x84, %%ah\n"
-        "mov  $1, %%dx\n"
-        "int  $0x15\n"
-        : "=ax"(a->x), "=bx"(a->y), "=cx"(b->x), "=dx"(b->y)
-        : /**/
-        : /**/);
+    asm volatile ("mov  $0x84, %%ah\n"
+                  "mov  $1, %%dx\n"
+                  "int  $0x15\n"
+                  : "=a"(a->x), "=b"(a->y), "=c"(b->x), "=d"(b->y)
+                  : /**/
+                  : /**/);
     joystick_limits[0].min = min(joystick_limits[0].min, a->x);
     joystick_limits[0].max = max(joystick_limits[0].max, a->x);
     joystick_limits[0].min = min(joystick_limits[0].min, a->y);
@@ -27,19 +26,19 @@ void joystick_read2(struct joystick *a, struct joystick *b)
     joystick_limits[1].min = min(joystick_limits[1].min, b->y);
     joystick_limits[1].max = max(joystick_limits[1].max, b->y);
     uint16_t buttons = 0;
-    asm("mov  $0x84, %%ah\n"
-        "mov  $0, %%dx\n"
-        "int  $0x15\n"
-        : "=ax"(buttons)
-        : /**/
-        : "%bx", "%cx", "%dx", "%flags");
+    asm volatile ("mov  $0x84, %%ah\n"
+                  "mov  $0, %%dx\n"
+                  "int  $0x15\n"
+                  : "=a"(buttons)
+                  : /**/
+                  : "bx", "cx", "dx", "flags");
     a->a = !(buttons & (1 << 4));
     a->b = !(buttons & (1 << 5));
     b->a = !(buttons & (1 << 6));
     b->b = !(buttons & (1 << 7));
 }
 
-void joystick_read(struct joystick *joystick)
+static void joystick_read(struct joystick *joystick)
 {
     struct joystick dummy;
     joystick_read2(joystick, &dummy);
