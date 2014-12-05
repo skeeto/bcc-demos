e81f36c0bcef5af991cd7fabe5b5ddf8de9235c9
Christopher Wellons <wellons@nullprogram.com>
2014-12-04 21:47:03 -0500

    Add port functions.

---
+++ gcc/port.h
@@ -0,0 +1,22 @@
+#ifndef PORT_H
+#define PORT_H
+
+static inline int16_t inportb(int16_t port)
+{
+    volatile uint8_t value;
+    asm volatile ("in   %1, %0\n"
+                  : "=r"(value)
+                  : "rm"(port)
+                  : /* no clobber */);
+    return value;
+}
+
+static inline void outportb(int16_t port, uint8_t value)
+{
+    asm volatile ("out   %%al, %%dx\n"
+                  : /* no output */
+                  : "d"(port), "a"(value)
+                  : /* no clobber */);
+}
+
+#endif
